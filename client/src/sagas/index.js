c607a828344e551ce69b7fac3e7d2f0b0e97dc71
brianmontero18 <brianmontero18@gmail.com>
2019-05-31 17:24:24 -0300

    Creating first sagas

--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -1,10 +1,16 @@
 import 'regenerator-runtime/runtime';
-import { all, takeLatest, put } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
 import * as Actions from '../actions';
 import { GET_QUERY_RESULT, } from '../appconstants';
+import { getQueryResult } from '../api';
 
-function* getQueryResult() {
-    yield put(Actions.getQueryResultSuccess)
+function* getQueryResult(action) {
+    try {
+        const result = yield call(getQueryResult, action.query);
+        yield put(Actions.getQueryResultSuccess(result));
+    } catch(error) {
+        yield put(Actions.getQueryResultsFail(error));
+    }
 }
 
 export default function* () {
