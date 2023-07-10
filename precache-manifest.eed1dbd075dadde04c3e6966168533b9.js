88e5a399e994be1316c59993f4ef39ac9c050bc4
brianmontero18 <brianmontero18@users.noreply.github.com>
2023-07-10 21:12:52 +0000

    deploy: 7681b7e04b40fcdcfaa9c044781fe9f7993bb196

---
+++ precache-manifest.eed1dbd075dadde04c3e6966168533b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2af44068ed3723f29371b2db1d04428",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b59c98c239d5fb0138e6",
+    "url": "/static/css/main.928637d5.chunk.css"
+  },
+  {
+    "revision": "c068437cd461caf15d06",
+    "url": "/static/js/2.c3e5dc2d.chunk.js"
+  },
+  {
+    "revision": "b59c98c239d5fb0138e6",
+    "url": "/static/js/main.1c88ea83.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
