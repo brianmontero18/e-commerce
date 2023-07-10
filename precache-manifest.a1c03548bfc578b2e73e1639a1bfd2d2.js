1f33294e102acfef7ce3e81e803dbf2e782d878d
brianmontero18 <brianmontero18@users.noreply.github.com>
2023-07-10 21:19:28 +0000

    deploy: 553f9ec5b3e3f0914bdeb5e7a206916380864ede

---
+++ precache-manifest.a1c03548bfc578b2e73e1639a1bfd2d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12887ba07f62bea85250b799264e5488",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "fe1e1595beadc49a3c06",
+    "url": "/e-commerce/static/css/main.928637d5.chunk.css"
+  },
+  {
+    "revision": "c068437cd461caf15d06",
+    "url": "/e-commerce/static/js/2.c3e5dc2d.chunk.js"
+  },
+  {
+    "revision": "fe1e1595beadc49a3c06",
+    "url": "/e-commerce/static/js/main.9656fbc4.chunk.js"
+  },
+  {
+    "revision": "290256ac1007569aecdb",
+    "url": "/e-commerce/static/js/runtime~main.8575550b.js"
+  }
+]);
